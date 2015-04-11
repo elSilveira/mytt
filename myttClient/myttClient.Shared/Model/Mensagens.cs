007ddb21812ebae8f5c450831f2f45784ae9855a
André Lima <andreloty@outlook.com>
2015-04-11 08:38:09 -0300

    Novas correções

---
+++ myttClient/myttClient.Shared/Model/Mensagens.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myttClient.Model
+{
+    public class Mensagens
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; }
+        public string Username { get; set; }
+        public string Data { get; set; }
+    }
+}
---
+++ myttClient/myttClient.Shared/Model/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myttClient.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Avatar { get; set; }
+        public string Bio { get; set; }
+        public string Website { get; set; }
+    }
+}
